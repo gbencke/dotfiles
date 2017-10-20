08383d364c5bc3d80f532c9ac9134f0fcdead454
gbencke <gbencke@gmail.com>
2017-10-20 17:40:36 -0200

    Adjustments on new machine script

--- new.host/new_script.sh
+++ new.host/new_script.sh
@@ -2,14 +2,11 @@
 
 sudo apt-get update;
 
-sudo apt-get install git build-essential gitk gdb tcl8.5 arandr curl tmux vim mc tig openssh* p7zip-full htop vim mc tig git make gcc build-essentials curl tmux wget python-pip
+sudo apt-get install net-tools git build-essential gitk gdb tcl8.5 arandr curl tmux vim mc tig openssh* p7zip-full htop vim mc tig git make gcc build-essential curl tmux wget python-pip
 sudo apt-get install language-pack-en-base 
 sudo dpkg-reconfigure locales
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash ;
-
-sudo apt-get install language-pack-en-base
-sudo dpkg-reconfigure locales
 
 sudo -E add-apt-repository ppa:webupd8team/java
 sudo -E add-apt-repository ppa:webupd8team/sublime-text-3 
@@ -19,6 +16,18 @@ sudo -E apt-get install oracle-java8-set-default
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+nvm install v4.6.0
+nvm use v4.6.0
+
+npm install -g bower gulp webpack angcli grunt sass 
+
+mkdir git
+cd git
+git clone https://github.com/gbencke/dotfiles.git
+
+
+
 
 
 
