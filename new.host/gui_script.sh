e72c482381633d4878d5aba28f21f80ed8f7c7dc
Guilherme Bencke <gbencke@gmail.com>
2017-12-19 10:27:06 -0200

    Added GUI instalation with chrome

---
+++ new.host/gui_script.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+sudo systemctl enable lightdm.service
+sudo service lightdm.service start
+
+sudo apt-get -y install xfce4 xfce4-goodies tightvncserver
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' 
+sudo apt-get update  
+sudo apt-get install google-chrome-stable  
+
+cp -f ~/git/dotfiles/vnc/xstartup ~/.vnc/xstartup
+
+vncserver -geometry 1440x990
+
+
