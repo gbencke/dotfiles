00080f6d664da98cf8dfdc3959adc2f52d63c931
gbencke <gbencke@benckesoftware.com.br>
2018-02-28 15:05:35 -0300

    Script to switch vimrc profiles and reset instalation

---
+++ switch_vimrc.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+if test "$#" -ne 1;
+then echo 'Need to specify the vimrc to use. Options are: regular, js, python' ; exit -1
+fi
+
+VIMRC_FILE=''
+if test "$1" == 'regular';
+then VIMRC_FILE=~/git/dotfiles/vimrc
+fi
+
+if test "$1" == 'js';
+then VIMRC_FILE=~/git/dotfiles/vimrc_javascript
+fi
+
+if test "$1" == 'python';
+then VIMRC_FILE=~/git/dotfiles/vimrc_python
+fi
+
+if test "$VIMRC_FILE" == '';
+then echo 'This vimrc was not recognized...'; exit -1;
+fi
+
+echo "Using $VIMRC_FILE"
+
+cp -f $VIMRC_FILE ~/.vimrc
+
+rm -rf ~/.vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
+
+
+if test "$1" == 'python';
+then cd ~/.vim/plugged/YouCompleteMe; ./install.sh
+fi
+
