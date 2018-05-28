1589c9f2a3dc59650b4525044a7bfa0f29109568
gbencke <gbencke@benckesoftware.com.br>
2018-05-27 22:00:34 -0300

    Standard npm install scripts

---
+++ npm/frontend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install v8.11.2
+nvm use v8.11.2
+
+npm install -g bower gulp webpack angcli grunt sass js-beautify typescript-formatter prettier
