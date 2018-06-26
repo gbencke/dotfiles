a8cea69ad88346dd3fc3216ffcb98de05e21b204
Guilherme Bencke <gbencke@gmail.com>
2018-06-26 18:25:48 -0300

    Ajustes finais no Arch

--- new.host/arch/arch.000.base.sh
+++ new.host/arch/arch.000.base.sh
@@ -23,16 +23,18 @@ vncserver
 
 mkdir -p git/000.INFRA
 chmod 755 -R git 
+chmod 755 -R /root
 cd git/000.INFRA
 git clone http://github.com/gbencke/dotfiles/
 mkdir -p ~/.config/termite
 cp ~/git/000.INFRA/dotfiles/new.host/arch/termite/config ~/.config/termite/config
-cp -r ~/git/000.INFRA/dotfiles/wallpaper ~/Wallpapers
+cp -r ~/git/000.INFRA/dotfiles/new.host/wallpaper ~/Wallpapers
 cp ~/git/000.INFRA/dotfiles/new.host/arch/vnc/xstartup ~/.vnc/xstartup
 cp ~/git/000.INFRA/dotfiles/new.host/arch/vnc/config ~/.vnc/config
 mkdir -p ~/.config/i3/
 cp ~/git/000.INFRA/dotfiles/new.host/arch/vnc/i3config ~/.config/i3/config
 cat ~/git/000.INFRA/dotfiles/shells/bashrc >> ~/.bashrc
+chmod 755 -R ~/git 
 
 vncserver -kill :1
 vncserver
@@ -41,5 +43,6 @@ passwd gbencke
 su gbencke
 cd
 ln -s /root/git ~/git 
+unset NVM_DIR
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 cat ~/git/000.INFRA/dotfiles/shells/bashrc >> ~/.bashrc
