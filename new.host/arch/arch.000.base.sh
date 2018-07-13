3b324d3fce8ddc154513a26c296be55ca503e72c
Guilherme Bencke <gbencke@gmail.com>
2018-07-13 14:26:44 -0300

    Added optional packages for ranger and also some identation fixes

--- new.host/arch/arch.000.base.sh
+++ new.host/arch/arch.000.base.sh
@@ -5,6 +5,7 @@
     sudo pacman -Sy --noconfirm sudo git curl tmux vim mc tig python2 python2-pip p7zip htop mc wget unzip zsh protobuf
     sudo pacman -Sy --noconfirm tree nano dos2unix bc python python-pip cmake graphviz python-h5py ctags 
     sudo pacman -Sy --noconfirm rsync ranger go compton virtualgl termite i3 i3status i3blocks sddm feh tigervnc ttf-inconsolata
+    sudo pacman -Sy --noconfirm w3m mediainfo libcaca highlight poppler unrar
 
     groupadd gbencke
     useradd -m -g gbencke  -s /bin/bash gbencke
@@ -15,8 +16,8 @@
     git config --global push.default simple
     git config --global core.editor vim
 
-    pip2 install autopep8 pylint virtualenv pmm cython pillow lxml
-    pip install autopep8 pylint virtualenv pmm cython pillow lxml
+    pip2 install autopep8 pylint virtualenv pmm cython pillow lxml pdftotext chardet
+    pip install autopep8 pylint virtualenv pmm cython pillow lxml pdftotext chardet 
 
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
@@ -40,10 +41,10 @@
     vncserver -kill :1
     vncserver
 
-#INTERACTIVE	
+#INTERACTIVE    
     passwd gbencke
     su gbencke
-	
+        
 #NONINTERACTIVE
     cd
     ln -s /var/git ~/git 
@@ -53,14 +54,14 @@
     git config --global user.name "Guilherme Bencke"  
     git config --global push.default simple
     git config --global core.editor vim
-	
+        
 #INTERACTIVE
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#NONINTERACTIVE	
+#NONINTERACTIVE 
     cat /var/git/000.INFRA/dotfiles/shells/bashrc >> ~/.bashrc
     cat /var/git/000.INFRA/dotfiles/shells/zshrc >> ~/.zshrc
     cp /var/git/000.INFRA/dotfiles/new.host/tmux/.tmux.conf ~/.tmux.conf
     sed -i -e 's/robbyrussell/clean/g' /home/gbencke/.zshrc
-$SHELL
+    $SHELL
 
