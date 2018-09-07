d92ce429425672c3d53ba41e48ca4d618e0aa5e8
gbencke <gbencke@benckesoftware.com.br>
2018-09-07 17:12:59 -0300

    Normalization of python and javascript vimrc files

--- vim/switch_vimrc.sh
+++ vim/switch_vimrc.sh
@@ -37,10 +37,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-if test "$1" == 'python';
-then cd ~/.vim/plugged/YouCompleteMe; ./install.sh
-fi
-
 if test "$1" == 'js';
 then nvm install v8.11.2 &&  npm install -g prettier typescript js-beautify gulp grunt 
 fi
