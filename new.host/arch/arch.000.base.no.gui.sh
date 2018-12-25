7729993b3f58984446daa6220177dc72713339aa
Guilherme Bencke <gbencke@benckesoftware.com.br>
2018-12-25 18:11:50 -0200

    Added jdk for the arch install script

--- new.host/arch/arch.000.base.no.gui.sh
+++ new.host/arch/arch.000.base.no.gui.sh
@@ -8,7 +8,7 @@
     sudo pacman -Sy --noconfirm sudo git curl tmux vim mc tig python2 python2-pip p7zip htop mc wget unzip zsh protobuf
     sudo pacman -Sy --noconfirm tree nano dos2unix bc python python-pip cmake graphviz python-h5py ctags 
     sudo pacman -Sy --noconfirm rsync ranger go scrot tidy
-    sudo pacman -Sy --noconfirm w3m mediainfo libcaca highlight poppler unrar shellcheck
+    sudo pacman -Sy --noconfirm w3m mediainfo libcaca highlight poppler unrar tidy shellcheck jdk-openjdk
 
     sudo systemctl disable man-db.service
     sudo systemctl disable man-db.timer
