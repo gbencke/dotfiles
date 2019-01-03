6ab5825269ce4cdd23805c63850e4f5f9ecac28f
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-01-03 10:16:38 -0200

    Forcing the update of pacman keys

--- new.host/arch/arch.000.base.no.gui.sh
+++ new.host/arch/arch.000.base.no.gui.sh
@@ -4,7 +4,7 @@
 #NONINTERACTIVE
     sudo pacman -Q archlinux-keyring
     sudo pacman-key --populate archlinux
-    sudo pacman -Syu glibc
+    sudo pacman -Sy --noconfirm archlinux-keyring && pacman -Syyu
     sudo pacman -Sy --noconfirm sudo git curl tmux vim mc tig python2 python2-pip p7zip htop mc wget unzip zsh protobuf
     sudo pacman -Sy --noconfirm tree nano dos2unix bc python python-pip cmake graphviz python-h5py ctags 
     sudo pacman -Sy --noconfirm rsync ranger go scrot tidy
