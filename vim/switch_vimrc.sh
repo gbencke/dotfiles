aea9770a135260735dfa70cfaffba13ef0b5687c
Guilherme <gbencke@benckesoftware.com.br>
2019-06-23 21:09:21 -0300

    Added arch js option

--- vim/switch_vimrc.sh
+++ vim/switch_vimrc.sh
@@ -2,7 +2,7 @@
 
 
 if test "$#" -ne 1;
-then echo 'Need to specify the vimrc to use. Options are: regular, js, python' ; exit -1
+then echo 'Need to specify the vimrc to use. Options are: regular, js, arch, python' ; exit -1
 fi
 
 VIMRC_FILE=''
@@ -12,6 +12,10 @@ fi
 
 if test "$1" == 'js';
 then VIMRC_FILE=./vimrc_javascript
+fi
+
+if test "$1" == 'arch';
+then VIMRC_FILE=./vimrc_javascript_arch
 fi
 
 if test "$1" == 'python';
@@ -37,7 +41,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-if test "$1" == 'js';
+if test "$1" == 'js' || test "$1" == "arch";
 then nvm install v8.11.2 &&  npm install -g prettier typescript js-beautify gulp grunt 
 fi
 
