5e89836864a09ae8a42ae54f3c9b786d2c93fd3f
gbencke <gbencke@benckesoftware.com.br>
2019-07-16 10:45:26 -0300

    Added more packages for linting

--- npm/frontend.sh
+++ npm/frontend.sh
@@ -10,6 +10,6 @@ nvm use v8.11.2
 npm install -g bower gulp webpack angcli grunt sass js-beautify 
 npm install -g typescript-formatter prettier markserv standard 
 npm install -g jsonlint fixjson htmlhint scss-lint sass-lint 
-npm install -g create-react-app create-react-native-app react-devtools
+npm install -g create-react-app react-native-cli react-devtools
 npm install -g sass scss-lint stylelint stylelint-config-standard
-npm install -g stylelint-config-standard
+npm install -g stylelint-config-standard prettier tslint
