380a5b4fe73e2fb85a7749dd24002be3cb2e7f4c
Guilherme Bencke <gbencke@benckesoftware.com.br>
2020-01-23 19:32:05 -0300

    Simplify arch install...

--- new.host/arch/arch.000.base.sh
+++ new.host/arch/arch.000.base.sh
@@ -2,16 +2,14 @@
 # Tested with Base AMI: ami-0f040c7d22aedeb27
 
 #NONINTERACTIVE
+    sudo su
     sudo pacman -Q archlinux-keyring
     sudo pacman-key --populate archlinux
-    sudo pacman -Sy --noconfirm archlinux-keyring && pacman -Syyu
-    sudo pacman -Sy --noconfirm sudo git curl tmux vim mc tig python2 python2-pip p7zip htop mc wget unzip zsh protobuf
-    sudo pacman -Sy --noconfirm tree nano dos2unix bc python python-pip cmake graphviz python-h5py ctags 
-    sudo pacman -Sy --noconfirm rsync ranger go compton virtualgl termite i3 i3status i3blocks sddm feh tigervnc ttf-inconsolata
-    sudo pacman -Sy --noconfirm w3m mediainfo libcaca highlight poppler unrar scrot tidy shellcheck jdk-openjdk
-
-    sudo systemctl disable man-db.service
-    sudo systemctl disable man-db.timer
+    sudo pacman -Sy --noconfirm archlinux-keyring 
+    sudo pacman -Sy --noconfirm sudo git curl tmux vim mc tig p7zip htop mc wget unzip zsh protobuf
+    sudo pacman -Sy --noconfirm tree nano dos2unix bc graphviz ctags 
+    sudo pacman -Sy --noconfirm rsync ranger compton virtualgl termite i3 i3status i3blocks sddm feh tigervnc ttf-inconsolata
+    sudo pacman -Sy --noconfirm w3m mediainfo libcaca highlight unrar scrot tidy shellcheck 
 
     groupadd gbencke
     useradd -m -g gbencke  -s /bin/bash gbencke
@@ -22,8 +20,8 @@
     git config --global push.default simple
     git config --global core.editor vim
 
-    pip2 install flake8 autopep8 pylint virtualenv pmm cython pillow lxml pdftotext chardet vim-vint
-    pip install flake8 autopep8 pylint virtualenv pmm cython pillow lxml pdftotext chardet vim-vint
+    #pip2 install flake8 autopep8 pylint virtualenv pmm cython pillow lxml pdftotext chardet vim-vint
+    #pip install flake8 autopep8 pylint virtualenv pmm cython pillow lxml pdftotext chardet vim-vint
 
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
