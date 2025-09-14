8f1589b9509275ade7f97d769698e28d76709dad
Guilherme Bencke <gbencke@users.noreply.github.com>
2025-09-14 19:50:44 -0300

    Feat/improvements prompting (#15)
    * fix:fix name for commands
    
    * feat: added one more context type
    
    * fix: fix and completed helper message
    
    * feat: added text styling
    
    * fix: fix paths
    
    * fix: fix context generation
    
    * fix: fix output file
    
    * feat: added a simple review
    
    * fix: fix syntax error
    
    ---------
    
    Co-authored-by: Guilherme Bencke <gbencke@bencksoftware.com.br>

--- prompts/prompts.sh
+++ prompts/prompts.sh
@@ -167,18 +167,83 @@ function _generate_pr_description(){
 
 function _review_pr_complete_instructions(){
   case $1 in
-      "stash" )
+      "staged" )
         SOURCE_PATH="${(%):-%x}"
         files=$( git diff --name-only --cached)
         files_=$(echo "$files" | paste -sd ',' -)
         code2prompt -O ./code2prompt.md -i $files_ . 
         cat ./code2prompt.md > ./review_pr_complete.md
-        cat "$(dirname -- $SOURCE_PATH)/./$2" >> ./review_pr_complete.md
+        cat "$(dirname -- $SOURCE_PATH)/./pr.guides/$2" >> ./review_pr_complete.md
       ;;
-      "stash_dependencies" | "all" )
+      "staged_dependencies" | "all" )
         code2prompt -O ./code2prompt.md -e review_pr_complete.md . 
-        cat "$(dirname -- $SOURCE_PATH)/./$2" >> ./review_pr_complete.md
+        cat "$(dirname -- $SOURCE_PATH)/./pr.guides/$2" >> ./review_pr_complete.md
         cat ./code2prompt.md >> ./review_pr_complete.md
+        return 1
+      ;;
+      "staged_diff" )
+        cat "$(dirname -- $SOURCE_PATH)/./pr.guides/$2" >> ./review_pr_complete.md
+        echo "**Input:**
+        - List of Changed Files: \n " >> review_pr_complete.md
+
+        git diff -U10 --staged --name-only >> review_pr_complete.md
+
+        echo " \n## Pull Request Review Description Template for LLM
+        - Relevant Diff or Code Snippet (optional): \n \n " >> review_pr_complete.md
+
+        git diff -U10 --staged  >> review_pr_complete.md
+        return 1
+      ;;
+
+      "staged_diff_simple" )
+        echo "# Pull Request Review Description Template for LLM
+
+        Use this template to prompt a Large Language Model (LLM) to generate an insightful, actionable, and well-structured pull request review description.
+        Please use english language and the output needs to be in markdown format.
+
+        ---
+
+        ## Prompt Template
+
+        You are an experienced software engineer and code reviewer. Given the following pull request details, generate a thorough, constructive, and actionable pull request review description. Be specific, clear, and concise.
+
+        **Instructions:**
+        Summarize the purpose and scope of the pull request in 1–2 sentences.
+        - List the main changes introduced, referencing relevant files or modules.
+        - List the main changes introduced, per changed file.
+        - Point out strengths or well-implemented aspects.
+        - Maintain a positive and professional tone.
+
+        **Input:**
+        - Pull Request Description: $(echo $1)
+        - List of Changed Files: \n " >> review_pr_complete.md
+
+        git diff -U10 --staged --name-only >> review_pr_complete.md
+
+        echo " \n## Pull Request Review Description Template for LLM
+        - Relevant Diff or Code Snippet (optional): \n \n " >> review_pr_complete.md
+
+        git diff -U10 --staged  >> review_pr_complete.md
+
+        echo " **Pull Request Review**
+
+        **Summary:**
+        - {{LLM generates a brief summary of the PRs purpose and scope in markdown format.}}
+
+        **Main Changes:**
+        - {{LLM lists and briefly explains the key changes in markdown format.}}
+
+        **Detailed Changes:**
+        - {{LLM describes the changes for each modified file in markdown format}}
+
+        ---
+
+        **Review Tone:**
+        - Supportive, clear, and focused on code quality and collaboration in markdown format.
+
+        ---
+
+        **End of Template**" >> review_pr_complete.md
         return 1
       ;;
       * )
@@ -224,17 +289,24 @@ function _find_git_root() {
 
 
 function _review_code_pr(){
-
   # Check if exactly 2 arguments are provided: files_to_be_included and template_file
   if [[ $# -lt 3 ]]; then
-    echo "Error: Please enter the template to use and also the files to be reviewed:"
-    echo "Usage: review_code_pr <<Context:[stash|stash_dependencies|all]>> <<Command file>> <<Persona file>>"
-    echo "Where:"
     echo ""
-    echo "Context:"
-    echo "Command:"
-    echo "Persona:"
-
+    echo -e "\033[31mError: Please enter the template to use and also the files to be reviewed:\033[0m"
+    echo ""
+    echo -e "\033[33mUsage: review_code_pr <<Context Window:[staged|staged_dependencies|staged_diff|all]>> <<Command file>> <<Persona file>>\033[0m"
+    echo -e "\033[34mWhere:\033[0m"
+    echo ""
+    echo -e "* \033[1mContext Window\033[0m: This is the type of context that will be used for this command"
+    echo ""
+    echo -e "* \033[1mCommand\033[0m: the command prompt, there are the following:"
+    echo -e "    \033[3mpr_guidelines_architecture\033[0m: general prompt for architectural review"
+    echo -e "    \033[3mpyramid_refactoring\033[0m: prompt for pyramid refactoring type of review"
+    echo ""
+    echo -e "* \033[1mPersona\033[0m: The characteristics of the person or guidelines:"
+    echo -e "    \033[3mbootstrap_prompt\033[0m: Complete guidelines"
+    echo -e "    \033[3mgood_general_guideline\033[0m: Simple guidelines"
+    echo ""
     return 1
   fi
 
@@ -242,7 +314,7 @@ function _review_code_pr(){
 
   SOURCE_PATH="${(%):-%x}"
   echo "" > ./review_pr_complete.md
-  cat "$(dirname -- $SOURCE_PATH)/./$3" >> ./review_pr_complete.md
+  cat "$(dirname -- $SOURCE_PATH)/./guidelines/$3" >> ./review_pr_complete.md
 
   _review_pr_complete_instructions $1 $2
   if [[ -z "./review_pr_complete.md" ]]; then
